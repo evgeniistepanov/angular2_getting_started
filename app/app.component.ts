cdcc37999518b32ac787dc86729d6c8afc7d9c84
Evgenii Stepanov <tird45@yandex.ru>
2016-05-23 18:27:56 +0300

    added simple User obj

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from 'angular2/core';
 import {WelcomeComponent} from './home/welcome.component'
+import {SimpleObj} from './simple-obj/simple-obj.component'
 
 @Component({
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
         <div>First Component</div>
+        <h2>{{simpleObj.username}}</h2>
     </div>
     <welcome></welcome>
     `,
@@ -14,4 +16,8 @@ import {WelcomeComponent} from './home/welcome.component'
 
 export class AppComponent {
     pageTitle: string = 'Acme product Managment';
+    simpleObj: SimpleObj;
+    constructor(){
+        this.simpleObj = new SimpleObj('TestUser', '1234431', 20)
+    }
 }
---
+++ app/simple-obj/simple-obj.component.ts
@@ -0,0 +1,11 @@
+export class SimpleObj {
+    username: string;
+    password: string;
+    age: number;
+    constructor(username: string, password: string, age: number){
+        this.username = username;
+        this.password = password;
+        this.age = age;
+    }
+}
+
