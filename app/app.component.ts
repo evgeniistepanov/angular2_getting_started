8d12b6933d22647abeefc42428d11b32c91ea674
= <=>
2016-05-23 16:48:39 +0300

    added welcome component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
-
 import { Component } from 'angular2/core';
+import {WelcomeComponent} from './home/welcome.component'
 
 @Component({
     selector: 'pm-app',
@@ -7,7 +7,9 @@ import { Component } from 'angular2/core';
     <div><h1>{{pageTitle}}</h1>
         <div>First Component</div>
     </div>
-    `
+    <welcome></welcome>
+    `,
+    directives: [WelcomeComponent]
 })
 
 export class AppComponent {
--- app/home/welcome.component.ts
+++ app/home/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component } from 'angular2/core';
 
 @Component({
+    selector: 'welcome',
     templateUrl: 'app/home/welcome.component.html'
 })
 export class WelcomeComponent {
